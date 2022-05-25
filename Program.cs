ddb3f4f73f1589a366b0b2db2839960c8340ad74
maryamze <105809184+maryamze@users.noreply.github.com>
2022-05-26 02:49:40 +0430

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,35 +1,27 @@
 ﻿using System;
 
-namespace asp.net
+namespace exercise
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // Hello world 
-              Console.WriteLine("Hello World !");
-           // variables
-              int age = 32;
-              string name= "maryamzeydabadi" ;
-           // string concatenation
-              Console.WriteLine("String Concatenation");
-              Console.WriteLine("Hello my name is " + name + ", Iam "+ age + "years old" );   
-           // string formating 
-               Console.WriteLine("String Formating");
-               Console.WriteLine("Hello my name is {0} , Iam {1}years old",name,age ); 
-           // string interpolation
-               Console.WriteLine("String Interpolation");
-               Console.WriteLine($"Hello my name is {name} , Iam {age}years old" ); 
-            // verbatim strings
-               Console.WriteLine("Verbatim Strings");
-               Console.WriteLine(@"Contrary to popular belief, Lorem Ipsum 
-                is not simply random text. It has roots in a piece of cla
-                ssical Latin literature from 45 BC, making it over 2000 years old. 
-                Richard McClintock, a Latin professor at Hampden-Sydney College in Virgini
-                a, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsu
-                m passage, and going through the cites of the word in classical literature, discovered the 
-                undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of"); 
-                Console.WriteLine(@"/Users/abtin_cheg/Desktop/C#/session1");
+            Console.WriteLine("please enter the count of series");
+            int number=Convert.ToInt32(Console.ReadLine());
+            for(int i=1;i<=number;i++){
+                for(int j=1;j<=number-i;j++){
+                   Console.Write(" ");
+                   Console.Write("\n");
+                }
+                for(int k=1;k<=i;k++){
+                     Console.Write($"{k}");
+                }
+                for(int m=i-1;m>=1;m--){
+                   Console.Write($"{m}");
+                }
+
+            }
+           // Console.Write("\n");
         }
     }
 }
