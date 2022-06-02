6dd3d7b61be993251240910ccbc0c9adbf46b5c3
maryamze <105809184+maryamze@users.noreply.github.com>
2022-06-03 03:07:22 +0430

    exercise3

--- Program.cs
+++ Program.cs
@@ -1,35 +1,43 @@
 ﻿using System;
-
-namespace asp.net
+using System.Collections.Generic;
+namespace exercise3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // Hello world 
-              Console.WriteLine("Hello World !");
-           // variables
-              int age = 32;
-              string name= "maryamzeydabadi" ;
-           // string concatenation
-              Console.WriteLine("String Concatenation");
-              Console.WriteLine("Hello my name is " + name + ", Iam "+ age + "years old" );   
-           // string formating 
-               Console.WriteLine("String Formating");
-               Console.WriteLine("Hello my name is {0} , Iam {1}years old",name,age ); 
-           // string interpolation
-               Console.WriteLine("String Interpolation");
-               Console.WriteLine($"Hello my name is {name} , Iam {age}years old" ); 
-            // verbatim strings
-               Console.WriteLine("Verbatim Strings");
-               Console.WriteLine(@"Contrary to popular belief, Lorem Ipsum 
-                is not simply random text. It has roots in a piece of cla
-                ssical Latin literature from 45 BC, making it over 2000 years old. 
-                Richard McClintock, a Latin professor at Hampden-Sydney College in Virgini
-                a, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsu
-                m passage, and going through the cites of the word in classical literature, discovered the 
-                undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of"); 
-                Console.WriteLine(@"/Users/abtin_cheg/Desktop/C#/session1");
+             List<employee> employees = new List<employee>();
+            //employee employee;
+            for (int i = 1; i <=5; i++)
+            {
+                employee employee= new employee();
+                Console.Write("Name  : ");
+                employee.Name = Console.ReadLine();
+                Console.Write("Haghzahmeh : ");
+                employee.payment = Convert.ToInt32(Console.ReadLine());
+                Console.Write("saatKArDarroz : ");
+                employee.hours = Convert.ToInt32(Console.ReadLine());
+                employees.Add(employee);
+                //Console.Write($"daramad mahiuane : {salary.paycalc(h, s)}");
+            }
+            foreach (employee item in employees)
+            {
+                Console.WriteLine($"Name  : {item.Name}");
+                Console.WriteLine($"Haghzahmeh : {item.payment}");
+                Console.WriteLine($"saatKArDarroz : {item.hours}");
+                Console.WriteLine($"daramad mahiuane : {item.paycalc(item.hours,item.payment)}");
+            }
+            Console.ReadKey();
+        }
+    }
+    public class employee
+    {
+        public string Name { get; set; }
+        public int hours { get; set; }
+        public int payment { get; set; }
+        public int paycalc(int hours, int basePayment)
+        {
+            return hours * basePayment * 24;
         }
     }
 }
